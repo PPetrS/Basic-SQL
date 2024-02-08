2cfc49665602b671404096805f989e4b42c51c5b
Petr Šimeček <petrinsim@gmail.com>
2024-02-08 22:04:57 +0100

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,5 +1,5 @@
 -- Search the database for cases where there is an EPZP or EPZP-man classification 
--- and sort them primarily by debt size and secondarily by date
+-- and sort them primarily by debt size and secondarily by date.
 
 SELECT  
 	eazv.c_zk, 
@@ -10,3 +10,11 @@ SELECT
 FROM eazv 
 WHERE eazv.vl IN ('EPZP','EPZP-man') 
 AND eazv.ind_pl=1 ORDER BY xeanoj(c_zk) DESC, date(eazv.t_stam_prn);
+
+
+-- Search all completed property auction cases in the last 10 days.
+SELECT  
+	c_zk 
+FROM eau 
+WHERE c_up IN (1062, 1607)
+AND EXISTS (SELECT * FROM eau eauEND WHERE eau.c_zk=eauEND.c_zk and eauEND.c_up=598 and eauEND.DT_UR>(today()-10));
